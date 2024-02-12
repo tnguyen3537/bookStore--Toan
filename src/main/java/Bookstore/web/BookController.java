4500e06388766497c96166f0956a5a5b6a6a4644
Toan Nguyen <tnguyen@3537gmail.com>
2024-02-11 20:17:16 -0600

    Chapter 3.3

--- src/main/java/Bookstore/web/BookController.java
+++ src/main/java/Bookstore/web/BookController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import Bookstore.domain.BookRespository;
+import Bookstore.domain.Book;
 
 @Controller
 public class BookController {
@@ -16,6 +19,24 @@ public class BookController {
 	public String getInfo(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
+	}
+
+	@GetMapping(value = "/delete/{id}")
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+
+	@GetMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+
+	@PostMapping(value = "/save")
+	public String save(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
 	}
 
 }
