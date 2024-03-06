22c301e9e9559f64bfdf26e83eae96736e0304d9
Toan Nguyen <tnguyen@3537gmail.com>
2024-03-06 05:13:55 -0600

    Chapter 6.1

--- src/main/java/Bookstore/web/BookController.java
+++ src/main/java/Bookstore/web/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,6 +31,7 @@ public class BookController {
 		return "booklist";
 	}
 
+	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping(value = "/delete/{id}")
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
