f294ccacaabf9a6849802ec1e0c0a5fedc4b9175
Toan Nguyen <tnguyen@3537gmail.com>
2024-03-30 15:50:59 -0500

    Revert "Chapter Testing"
    This reverts commit c162ed9da1f76c2ff2dcdcf57da3070fec807424.

--- src/main/java/Bookstore/domain/BookRespository.java
+++ src/main/java/Bookstore/domain/BookRespository.java
@@ -1,11 +1,7 @@
 package Bookstore.domain;
 
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRespository extends CrudRepository <Book,Long> {
-
-    List<Book> findByAuthor(String author);
 
 }
